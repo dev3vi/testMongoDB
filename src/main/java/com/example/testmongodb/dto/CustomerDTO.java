0e9c2b027afaf67e72aaf804da80aab371930791
BVCN 88 <thaoluong5343>
2022-05-17 15:14:34 +0700

    a

---
+++ src/main/java/com/example/testmongodb/dto/CustomerDTO.java
@@ -0,0 +1,11 @@
+package com.example.testmongodb.dto;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+public class CustomerDTO {
+
+    private String name;
+    private String love;
+
+}
